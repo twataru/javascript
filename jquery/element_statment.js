2515d60372b055efe3f002868254c99b95e440c1
taniuchi.wataru <taniuchi.wataru@unisoft.co.jp>
2017-03-03 18:50:28 +0900

    javascript first commit

---
+++ jquery/element_statment.js
@@ -0,0 +1,31 @@
+//$('element').method();
+$( '#hoge' ).on( 'click hover', function(){
+ alert( "hoge" );
+});
+
+
+/**
+* $.map
+*
+* 配列の各値を処理して新しい配列を作成する。
+*/
+
+//チェックが入っている要素のvalueを取得
+var checkBoxArray = $( '#checkbox :checked' ).map( function(){
+ return $( this ).val();
+}).get();
+
+/**
+* $.prop
+* $.attr
+*
+* 属性プロパティに値を設定、または取得
+*/
+
+//チェックを設定
+$( '#checkbox input:checkbox' ).prop( 'checked', true );
+
+//propとattrの挙動の違い
+var is_checked   = $('#check1').prop('checked');  //trueが返る ( 真偽値 )
+var attr_checked = $('#check1').attr('checked');  //'checked'が返る( 文字列 )
+
