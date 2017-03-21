992ae81afc105d0f530890e1fab667fbbf3ecbf3
taniuchi.wataru <taniuchi.wataru@unisoft.co.jp>
2017-03-21 14:26:35 +0900

    add jquery-ui-dialog

--- jquery/jquery-ui_statments.js
+++ jquery/jquery-ui_statments.js
@@ -2,12 +2,26 @@
  * jquery-ui-tooltip
  */
 //<div title="this is tooltip">this is div</div>
-$('div').tooltip();
+$( 'div' ).tooltip();
 
 //linebreak
 //<div title="this is tooltip.<br />doing an line break.">this is div</div>
-$('div').tooltip({
-   content:function(){
-        return $(this).attr("title");
-    } 
+$( 'div' ).tooltip( {
+    content: function(){
+        return $( this ).attr( "title" );
+    }
+} );
+
+/*
+ * jquery-ui-dialog
+ */
+//<input type="button" id="openDialog" value="open" />
+//<div id="dialog">this is dialog</div>
+$( '#dialog' ).dialog( {
+    autoOpen: false,
+    width: 500,
+    height: 500
+} );
+$('#openDialog').on('click',function(){
+   $( '#dialog' ).dialog( 'open' ); 
 });
